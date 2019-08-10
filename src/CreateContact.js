efa78b23ff8c03dca4e5ba0246ebd98056e7a4aa
Nitin3993 <nitinporwal99@gmail.com>
2019-08-10 14:26:44 +0530

    app completed with create functionality

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -4,12 +4,20 @@ import ImageInput from './ImageInput';
 
 import { Link } from 'react-router-dom';
 
+import serializeForm from 'form-serialize'
+
 class CreateContact extends Component {
+    handleSubmit= (e)=> {
+        e.preventDefault()
+        const values = serializeForm(e.target, {hash: true})
+        if(this.props.onCreateContact)
+            this.props.onCreateContact(values)
+    }
     render(){
         return(
             <div>
                 <Link className='close-create-contact' to = '/'>Close</Link>
-                <form className='create-contact-form'>
+                <form onSubmit={this.handleSubmit} className='create-contact-form'>
                     <ImageInput 
                         className='create-contact-avatar-input'
                         name='avatarURL'
